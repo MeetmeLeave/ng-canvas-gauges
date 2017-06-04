76846afdfa2612bf9ee254657ff38ae07c451728
VladM <vladimir.martynenko.work@gmail.com>
2017-06-04 18:50:04 +0300

    - updated import for the components in the example

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {LinearGaugeComponent, RadialGaugeComponent} from '../../node_modules/ng-canvas-gauges/component';
+import {LinearGaugeComponent, RadialGaugeComponent} from 'ng-canvas-gauges/component';
 
 @NgModule({
   declarations: [
