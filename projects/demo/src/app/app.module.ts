0c2c87deec011295fa6b9792a2792a7a8a271c50
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2019-03-01 18:14:19 -0600

    Improved with additional test cases for options & value updates

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GaugesModule } from 'projects/ng-canvas-gauges/src/public_api';
+import { GaugesModule } from 'ng-canvas-gauges';
 
 
 @NgModule({
